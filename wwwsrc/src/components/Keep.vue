9df16a05b2fc55bf25efc4a181053854ff802c37
Gerax Sotelo <gsotelo23@gmail.com>
2020-02-08 18:53:53 -0700

    Create keep component

---
+++ wwwsrc/src/components/Keep.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="keep">
+    <div class="card">
+      <img :src="keepData.img" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">{{keepData.name}}</h5>
+        <p class="card-text">{{keepData.description}}</p>
+        <a href="#" class="btn btn-primary">Go</a>
+        <div class="d-flex justify-content-around">
+          <span>Views: {{keepData.views}}</span>
+          <span>Shares: {{keepData.shares}}</span>
+          <span>Keeps: {{keepData.keeps}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Keep",
+  props: ["keepData"],
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.card {
+  max-width: 18rem;
+  box-shadow: 5px 15px 10px rgba(0, 0, 0, 0.15);
+  transition: 0.25s ease;
+}
+.card:hover {
+  box-shadow: 5px 15px 30px rgba(0, 0, 0, 0.3);
+  transform: scale(1.03);
+}
+.card-img-top {
+  max-width: 18rem;
+  /* max-height: 18rem; */
+}
+.card,
+.card-img-top,
+.card-body {
+  border-radius: 10px;
+}
+</style>
--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -7,25 +7,14 @@
     </div>
     <div class="row">
       <div class="card-container col-6 col-md-3" v-for="keep in publicKeeps" :key="keep.id">
-        <div class="card">
-          <img :src="keep.img || 'https://via.placeholder.com/150'" class="card-img-top" />
-          <div class="card-body">
-            <h5 class="card-title">{{keep.name}}</h5>
-            <p class="card-text">{{keep.description}}</p>
-            <a href="#" class="btn btn-primary">Go</a>
-            <div class="d-flex justify-content-around">
-              <span>Views: {{keep.views}}</span>
-              <span>Shares: {{keep.shares}}</span>
-              <span>Keeps: {{keep.keeps}}</span>
-            </div>
-          </div>
-        </div>
+        <Keep :keepData="keep" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Keep from "@/components/Keep.vue";
 export default {
   name: "home",
   mounted() {
@@ -40,27 +29,12 @@ export default {
     logout() {
       this.$store.dispatch("logout");
     }
+  },
+  components: {
+    Keep
   }
 };
 </script>
 
 <style scoped>
-.card {
-  max-width: 18rem;
-  box-shadow: 5px 15px 10px rgba(0, 0, 0, 0.15);
-  transition: 0.25s ease;
-}
-.card:hover {
-  box-shadow: 5px 15px 30px rgba(0, 0, 0, 0.3);
-  transform: scale(1.03);
-}
-.card-img-top {
-  max-width: 18rem;
-  /* max-height: 18rem; */
-}
-.card,
-.card-img-top,
-.card-body {
-  border-radius: 10px;
-}
 </style>
